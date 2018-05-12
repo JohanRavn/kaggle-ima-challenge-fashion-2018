Added standalone keras, this actually fixes multi_gpu_training. Linear scaling across gpus

src/dataset.py
@@ -18,7 +18,7 @@ def create_targets(anno):
 def create_image(path):
     img = cv2.imread(path)
     try:
-        img = cv2.resize(img, (224, 224))
+        img = cv2.resize(img, (299, 299))
     except cv2.error as e:
         print(e)
         print(path)
@@ -62,7 +62,7 @@ def convert_to_categorical(Y, num_classes):
         new_y.append(tmp)
     return new_y
 
-def calculate_num_classes(Y)
+def calculate_num_classes(Y):
     unique_targets = set()
     for target in Y:
         unique_targets = unique_targets.union(target)
@@ -70,13 +70,13 @@ def calculate_num_classes(Y)
     num_classes = max(unique_targets)
     return num_classes
 
-def load_data(dataset, selected):
-    file_count = 15000
+def load_data(dataset):
+    file_count = 2000
     f = open("../input/" + dataset + ".json", "r")
     labels = json.loads(f.read())
     anno = [x for x in labels["annotations"]][:file_count]
 
-    extraced
+    #extraced
 
     cpu_count = 14
     files_per_cpu = int(len(anno) / cpu_count) + 1
@@ -99,7 +99,7 @@ def load_data(dataset, selected):
         X += X_q.get()
         Y += Y_q.get()
 
-    num_classes = calculate_num_classes(num_classes)
+    num_classes = calculate_num_classes(Y)
     Y = convert_to_categorical(Y, num_classes)
     X = np.array(X).astype("float32")
     mean_pixel = [103.939, 116.779, 123.68]

src/train.py
@@ -2,7 +2,11 @@ import tensorflow as tf
 import numpy as np
 from dataset import load_data, load_test_data#, generate_results_report
 from sklearn.metrics import fbeta_score
-
+from keras.applications import Xception
+from keras.utils import multi_gpu_model
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.models import Model
+from keras.optimizers import Adam
 #config = tf.ConfigProto()
 #config.gpu_options.allow_growth = True
 #sess = tf.Session(config=config)
@@ -39,28 +43,50 @@ def train():
     X_train, y_train, num_classes = load_data("train")
     print(X_train.shape)
     print(y_train.shape)
-    base_model = tf.keras.applications.Xception(
+
+    """base_model = Xception(
         include_top=False,
         weights='imagenet',
-        input_shape=(224, 224, 3),
-        pooling='avg',
+        # input_shape=(299, 299, 3),
+        # pooling='avg',
         classes=num_classes)
     x = base_model.output
-    pred = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
-    model = tf.keras.Model(inputs=base_model.input, outputs=pred)
+    x = GlobalAveragePooling2D()(x)
+    pred = Dense(num_classes, activation='softmax')(x)
+    model = Model(inputs=base_model.input, outputs=pred)
 
-    adam = tf.keras.optimizers.Adam(lr=0.0001)
+    adam = Adam(lr=0.0001)
     model.compile(optimizer=adam,
                   loss="categorical_crossentropy",
                   metrics=['accuracy', f_score])
+
+
+
+
     model.fit(x=X_train,
               y=y_train,
-              batch_size=32,
+              batch_size=16,
               epochs=40,
               verbose=1,
               validation_split=0.3,
               shuffle=True
-              )
+              )"""
+
+    with tf.device('/cpu:0'):
+        model = Xception(weights=None,
+                         input_shape=(299, 299, 3),
+                         classes=num_classes)
+
+    # Replicates the model on 8 GPUs.
+    # This assumes that your machine has 8 available GPUs.
+    parallel_model =  multi_gpu_model(model, gpus=2, cpu_merge=True, cpu_relocation=True)
+    parallel_model.compile(loss='categorical_crossentropy',
+                           optimizer='rmsprop')
+    # This `fit` call will be distributed on 8 GPUs.
+    # Since the batch size is 256, each GPU will process 32 samples.
+    parallel_model.fit(X_train, y_train, epochs=20, batch_size=32)
+
+
     tf.keras.models.save_model(model, "../models/model")
     #X_test, labels = load_test_data()
     #result = model.predict(X_test)